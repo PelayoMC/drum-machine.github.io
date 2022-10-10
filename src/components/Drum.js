e059ef2ec0e3457d3ce80bcec69332167f165b8c
pelayo <pelayo.martinez@innvel.com>
2022-10-10 11:11:45 +0200

    terminada app

--- src/components/Drum.js
+++ src/components/Drum.js
@@ -1,37 +1,110 @@
-import { createRef, useEffect } from "react";
+import { createRef, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFreeCodeCamp } from "@fortawesome/free-brands-svg-icons";
 import DrumPad from "./Keys/DrumPad";
 import keys from "./Keys/keys";
 
 const Drum = () => {
-    const drumRef = createRef();
-    useEffect(() => {
-        if (drumRef) {
-            drumRef.current.focus();
-        }
-      }, [drumRef]);
-
-    const showData = event => {
-        console.log(event);
+  let [data, setData] = useState("");
+  let [power, setPower] = useState(true);
+  let [bank, setBank] = useState(false);
+  let [volume, setVolume] = useState(30);
+  let [key, setKey] = useState({ k: null, n: null });
+  const drumRef = createRef();
+  useEffect(() => {
+    if (drumRef) {
+      drumRef.current.focus();
     }
-    const keyPressed = evt => {
-        console.log(evt.key);
-    }
+  }, [drumRef]);
 
-    return <div id="drum-machine" className="row" ref={drumRef} onKeyDown={keyPressed} tabIndex={-1}>
-        <div id="drum-buttons" className="col-6">
-            {keys.map((k, index) => <DrumPad key={index} info={k} onSetValue={data => showData(data)}/>)}
+  const showData = (event) => {
+    setData(event);
+  };
+  const keyPressed = (evt) => {
+    setKey({ k: evt.key, n: evt.timeStamp });
+  };
+  const powerModifier = () => {
+    setPower(!power);
+  };
+  const bankModifier = () => {
+    setBank(!bank);
+  };
+  const volumeModifier = (evt) => {
+    console.log(evt.target.valueAsNumber);
+    setData("Volumen: " + evt.target.valueAsNumber);
+    setVolume(parseInt(evt.target.valueAsNumber));
+  };
+
+  return (
+    <div
+      id="drum-machine"
+      className="row"
+      ref={drumRef}
+      onKeyDown={keyPressed}
+      tabIndex={-1}
+    >
+      <div id="drum-buttons" className="col-6">
+        {keys.map((k, index) => (
+          <DrumPad
+            key={index}
+            power={power}
+            bank={bank}
+            volume={volume}
+            keyPressed={key}
+            info={k}
+            onSetValue={(data) => showData(data)}
+          />
+        ))}
+      </div>
+      <div className="col-6">
+        <div className="logo">
+          <span>FCC </span>
+          <FontAwesomeIcon icon={faFreeCodeCamp} size="lg" />
         </div>
-        <div className="col-6">
-        <div className="logo">logo</div>
         <div className="controls-container">
-            <div>select</div>
-            {/* <audio controls><source src="../../public/audio/Heater 1.mp3" type="audio/mp3"></source></audio> */}
-            <div id="display">zone name</div>
-            <div>slider</div>
-            <div>select2</div>
+          <label className="form-check-label" htmlFor="switch1">
+            Power
+          </label>
+          <div className="form-switch pb-1">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="switch1"
+              checked={power}
+              onChange={powerModifier}
+            ></input>
+          </div>
+          <div className="mb-1" id="display">
+            {power ? data : "POWER OFF"}
+          </div>
+          <div className="pb-3 pt-1">
+            <input
+              type="range"
+              value={volume}
+              min={0}
+              max={100}
+              step={1}
+              onChange={(event) => {
+                volumeModifier(event);
+              }}
+            />
+          </div>
+          <label className="form-check-label" htmlFor="switch2">
+            Bank
+          </label>
+          <div className="form-switch pb-1">
+            <input
+              className="form-check-input"
+              type="checkbox"
+              id="switch2"
+              checked={bank}
+              onChange={bankModifier}
+            ></input>
+          </div>
         </div>
-        </div>
+      </div>
     </div>
-}
+  );
+};
 
 export default Drum;
--- src/components/Keys/DrumPad.js
+++ src/components/Keys/DrumPad.js
@@ -1,27 +1,58 @@
-import PropTypes from 'prop-types';
-import { createRef } from 'react';
+import PropTypes from "prop-types";
+import { createRef, useEffect } from "react";
 
-const DrumPad = props => {
-    let { info, onSetValue } = props
+const DrumPad = (props) => {
+  let { info, power, bank, volume, keyPressed, onSetValue } = props;
 
-    const ref = createRef();
-    const playSound = rff => {
-        rff.current.play()
-        onSetValue(info.text);
+  useEffect(() => {
+    if (keyPressed.k && keyPressed.k.toUpperCase() === info.value) {
+      playSound();
     }
-    
-    return <button className="drum-pad" onClick={() => playSound(ref)} ><audio ref={ref} id={info.value} src={info.audio} type="audio/mpeg"></audio><b>{info.value}</b></button>
-// }}><audio className='clip' ref={ref} id={info.value}><source src={info.audio} type="audio/mpeg" /></audio><b>{info.value}</b></button>
-}
+    if (volume) {
+      let n = volume / 100;
+      ref.current.volume = n;
+    }
+  });
+
+  const ref = createRef();
+  const playSound = () => {
+    if (power) {
+      ref.current.load();
+      ref.current.play();
+      onSetValue(bank ? info.text2 : info.text);
+    }
+  };
+
+  return (
+    <button className="drum-pad" onClick={playSound}>
+      <audio
+        ref={ref}
+        id={info.value}
+        src={bank ? info.audio2 : info.audio}
+        type="audio/mpeg"
+      ></audio>
+      <b>{info.value}</b>
+    </button>
+  );
+  // }}><audio className='clip' ref={ref} id={info.value}><source src={info.audio} type="audio/mpeg" /></audio><b>{info.value}</b></button>
+};
 
 DrumPad.propTypes = {
-    info: PropTypes.object,
-    onSetValue: PropTypes.func
-}
+  info: PropTypes.object,
+  power: PropTypes.bool,
+  bank: PropTypes.bool,
+  volume: PropTypes.number,
+  keyPressed: PropTypes.object,
+  onSetValue: PropTypes.func,
+};
 
 DrumPad.defaultProps = {
-    info: {},
-    onSetValue: () => {}
-}
+  info: {},
+  power: false,
+  bank: false,
+  volume: 30,
+  keyPressed: {},
+  onSetValue: () => {},
+};
 
-export default DrumPad
+export default DrumPad;
